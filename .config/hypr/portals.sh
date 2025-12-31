2ee860e6d0b2296e056f08d9afbe8eb29e6e772c
jab <68988117+jablank@users.noreply.github.com>
2025-12-31 02:00:38 -0700

    0.53 cont. + hyprlauncher

--- .config/hypr/portals.sh
+++ .config/hypr/portals.sh
@@ -6,3 +6,9 @@ killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
 /usr/lib/xdg-desktop-portal &
+# 1. Force the env variables into the session
+exec-once = dbus-update-activation-environment --systemd --all
+
+# 2. Use 'gentle' restarts for the portals
+exec-once = systemctl --user restart xdg-desktop-portal-hyprland
+exec-once = systemctl --user restart xdg-desktop-portal
